8078e50ad1ac1e145acfe3122ed99ad05af60eb4
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-01-25 20:47:20 -0300

    Finished backend video

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const routes = require('./routes');
+
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://tindev:tindev@cluster0-aq2sz.mongodb.net/tindev?retryWrites=true&w=majority', {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+})
+
+server.use(cors());
+
+server.use(express.json());
+
+server.use(routes);
+
+server.listen(3333);
